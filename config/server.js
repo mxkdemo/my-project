b76542179510d6e356f94cd86807fc8f63ac0520
Mike <mjshort88@googlemail.com>
2021-08-09 11:49:58 +0100

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1f9d8221daa45550e36510c3f97114fa'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd705ea34-618f-4ed8-9d53-1264d51d75ea'
+};
